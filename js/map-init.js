9ca4b8f61f2e246a10155f55b436cb572e6734cd
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2018-03-22 15:35:56 +0300

    Tried to create a circle

--- js/map-init.js
+++ js/map-init.js
@@ -5,4 +5,18 @@ ymaps.ready(function init () {
     map.controls.remove('fullscreenControl');
     map.controls.remove('trafficControl');
     map.controls.remove('searchControl');
+    map.controls.remove('geolocationControl');
+
+    ymaps.geolocation.get().then(function (result) {
+        var myPos = result.get(0).geometry.position;
+        var searchCircle = new ymaps.geoObject({
+            geometry: {
+                type: 'Circle',
+                coordinates: myPos,
+                radius: 3500
+            }
+        })
+    }, function (error) {
+        console.log(error);
+    })
 });
