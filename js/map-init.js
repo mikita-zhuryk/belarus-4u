07ac5fe2f13f628b03d6489369cfd0a3f9e27cc2
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2018-03-22 20:24:08 +0300

    Fixed (probably) some stuff with colors

--- js/map-init.js
+++ js/map-init.js
@@ -1,24 +1,6 @@
-"use strict"
-let map;
-ymaps.ready(function init () {
-    map = new ymaps.Map("map-show", {center: [53.90843169, 27.55994617], zoom: 7})
-    map.controls.remove('fullscreenControl');
-    map.controls.remove('trafficControl');
-    map.controls.remove('searchControl');
-    map.controls.remove('geolocationControl');
-
-    ymaps.geolocation.get().then(function (result) {
-        console.log('Got location!');
-        let myPos = result.get(0).geometry["coordinates"];
-        console.log(typeof(myPos));
-        var searchCircle = new ymaps.geoObject({
-            geometry: {
-                type: 'Circle',
-                coordinates: myPos,
-                radius: 3500
-            }
-        })
-    }, function (error) {
-        console.log(error);
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: {lat: 53.67253, lng: 28.0726279}
     })
-});
+}
