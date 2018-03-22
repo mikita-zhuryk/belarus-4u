0b645712488c6dd2a7dc3fb3caba61e4e80ea0be
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2018-03-22 20:01:48 +0300

    1

--- js/map-init.js
+++ js/map-init.js
@@ -1,5 +1,5 @@
 "use strict"
-var map;
+let map;
 ymaps.ready(function init () {
     map = new ymaps.Map("map-show", {center: [53.90843169, 27.55994617], zoom: 7})
     map.controls.remove('fullscreenControl');
@@ -8,7 +8,9 @@ ymaps.ready(function init () {
     map.controls.remove('geolocationControl');
 
     ymaps.geolocation.get().then(function (result) {
-        var myPos = result.get(0).geometry.position;
+        console.log('Got location!');
+        let myPos = result.get(0).geometry["coordinates"];
+        console.log(typeof(myPos));
         var searchCircle = new ymaps.geoObject({
             geometry: {
                 type: 'Circle',
