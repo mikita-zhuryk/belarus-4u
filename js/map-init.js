d680cf04f17a14a3353a4f2e2c4641350b1a9b58
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2018-03-22 21:55:49 +0300

    Made geolocation work

--- js/map-init.js
+++ js/map-init.js
@@ -1,6 +1,38 @@
 function initMap() {
-    var map = new google.maps.Map(document.getElementById("map"), {
+    let flag;
+    let mapOptions = {
         zoom: 7,
-        center: {lat: 53.67253, lng: 28.0726279}
-    })
+        center: { lat: 53.67253, lng: 28.0726279 },
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: false
+    };
+    let map = new google.maps.Map(document.getElementById("map"), mapOptions);
+    var promise = new Promise(function (resolve, reject) {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                function (position) {
+                    mapOptions.center = { lat: position.coords.latitude, lng: position.coords.longitude };
+                    console.log(mapOptions.center);
+                    flag = true;
+                    resolve("Promise fullfilled!");
+                },
+                function () {
+                    reject("Problem geolocating");
+                })
+        }
+    });
+    promise.then(
+        result => {
+            map.setCenter(mapOptions.center);
+            console.log(map.center.lat() + ' ' + map.center.lng());
+            let point = new google.maps.InfoWindow({ map: map });
+            point.setPosition(mapOptions.center);
+            point.setContent("Centered on you");
+            console.log(result);
+        },
+        error => {
+            console.log("Geolocation error " + error);
+        }
+    );
 }
