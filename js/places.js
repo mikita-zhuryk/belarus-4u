59f0402b02eeece27b988b37dfd2c5e528a5e5dc
Mikita Zhuryk <mikita.zhuryk@gmail.com>
2018-03-31 10:13:01 +0300

    Search is kinda working

--- js/places.js
+++ js/places.js
@@ -1,31 +1,50 @@
 var service;
+var flag = false;
+var places;
 
-$("*").click(function search() {
-    var request = {
-        location: mapOptions.center,
-        radius: '3500',
-        types: ['cafe']
-    };
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+$(document).ready(function () {
+    $("a").click(function search() {
+        console.log($(this).text());
+        var request = {
+            location: mapOptions.center,
+            radius: '3500',
+            type: [$(this).text()]
+        };
+        if (!flag) service = new google.maps.places.PlacesService(map);
+        service.nearbySearch(request, callback);
+    })
 })
 
 function callback(results, status) {
     console.log("Everything kinda alright");
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      for (var i = 0; i < results.length; i++) {
-        var place = results[i];
-        createMarker(results[i]);      
-      }
+        if (flag) {
+            console.log("deleting markers");
+           for (var i = 0; i < places.length; i++) {
+               places[i].setMap(null);
+               places[i] = null;
+           }
+           places.length = 0;
+        }
+        if (!flag) {
+            var circle = new google.maps.Circle({
+                map: map,
+                center: mapOptions.center,
+                radius: 3500
+            })
+            flag = true;
+        }
+        places = results;
+        console.log(places[0].name);
+        for (var i = 0; i < places.length; i++) {
+            var place = places[i];
+            places[i] = new google.maps.Marker({
+                map: map,
+                position: place.geometry.location
+            });
+        }
     }
     else {
         console.log("We are fucked");
     }
-}
-
-function createMarker(place) {
-    var marker = new google.maps.Marker( {
-        map: map,
-        position: place.geometry.location
-    });
 }
