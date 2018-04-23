c0bb5b3650d1c203c7be684eaebe679bbb961da0
Vitaly Vasilevich <vitalyj.vasilevich@gmail.com>
2018-04-23 12:01:14 +0200

    tmp for pull

--- js/search.js
+++ js/search.js
@@ -314,11 +314,11 @@ $(document).ready(function () {
 })
 
 //$(document).ready(function (){
-$('.listNode').click(function () {
+/*$('.listNode').click(function () {
     var text = $(this).text();
     alert(text);
     document.cookie = "seen=text; expires=18/04/2020 00:00:00;";
-});
+});*/
 //});
 
 $(document).ready(function () {
@@ -510,3 +510,5 @@ function addHint(marker) {
         infoWnd.close(marker);
     })
 }
+
+$.ajax()
