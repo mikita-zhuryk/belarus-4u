f6165ac1d7eae181cfc9069781476ef79e3649c1
AlehLiazhaika <aleh.liazhaika@gmail.com>
2018-05-24 17:20:38 +0300

    Added themes

--- js/settings.js
+++ js/settings.js
@@ -42,3 +42,35 @@ $(document).ready(function () {
         changeLoc(loc);
     })
 })
+
+$(document).ready(function () {
+    $('.theme-btn').click(function (e) {
+        var value = $(this).val();
+        var root = document.querySelector(':root');
+        switch (value) {
+            case "Black": {
+                root.style.setProperty('--color0', '#181818');
+                root.style.setProperty('--color1', '#212222');
+                root.style.setProperty('--color2', 'rgb(130, 57, 255)');
+                root.style.setProperty('--color3', 'rgb(78, 28, 167)');
+                root.style.setProperty('--color4', 'rgb(61, 26, 128)');
+                root.style.setProperty('--color5', 'rgb(48, 20, 100)'); 
+                root.style.setProperty('--textColor', '#eceae4');
+                root.style.setProperty('--fontFamily', 'Poppins');
+                break;
+            }
+            case "White": {
+                root.style.setProperty('--color0', '#DFDFDF');
+                root.style.setProperty('--color1', '#F5F5F5');
+                root.style.setProperty('--color2', '#FF785B');
+                root.style.setProperty('--color3', '#FF5950');
+                root.style.setProperty('--color4', '#FFA292');
+                root.style.setProperty('--color5', '#ff473d');  
+                root.style.setProperty('--textColor', '#282828');
+                root.style.setProperty('--fontFamily', 'Poppins');                                              
+                break;
+            }
+        }
+        changeTheme([palette]);
+    })
+})
