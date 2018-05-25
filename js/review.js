aed4cdd87d49341d6a60c19d4be9bfb0bb36f4e6
Vitaly Vasilevich <vitalyj.vasilevich@gmail.com>
2018-05-25 23:59:46 +0200

    server first versin

--- js/review.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('#leaveReview').click(function () {
-        var review = document.getElementById('userReview').value;
-        console.log(review);
-    })
-})
--- js/thems.js
+++
@@ -1,4 +0,0 @@
-function changeTheme(palette) {
-    var lines = palette.split('\n', 6);
-    $('.menu-item')
-}
